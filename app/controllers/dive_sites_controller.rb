ee135458726a631a77a7d905cccbb7d5be71af7d
Nic Rothquel <nrothquel@gmail.com>
2022-08-23 16:37:48 -0400

    completed map and autocomplete

--- app/controllers/dive_sites_controller.rb
+++ app/controllers/dive_sites_controller.rb
@@ -1,6 +1,13 @@
 class DiveSitesController < ApplicationController
   def index
     @dive_sites = DiveSite.all
+    @markers = @dive_sites.geocoded.map do |dive_site|
+      {
+        lat: dive_site.latitude,
+        lng: dive_site.longitude,
+        info_window: render_to_string(partial: "info_window", locals: {dive_site: dive_site})
+      }
+    end
   end
 
   def show
