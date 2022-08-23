cece6d62c4b0a1a8a9f016ae2a59ce34e84b90b4
Nic Rothquel <nrothquel@gmail.com>
2022-08-23 15:16:22 -0400

    set up create dive site form

--- app/controllers/dive_sites_controller.rb
+++ app/controllers/dive_sites_controller.rb
@@ -1,30 +1,29 @@
 class DiveSitesController < ApplicationController
   def index
-    @dive_sites = Dive_site.all
+    @dive_sites = DiveSite.all
   end
 
   def show
-    @dive_site = Dive_site.find(params[:id])
+    @dive_site = DiveSite.find(params[:id])
   end
 
   def new
-    @dive_site = Dive_site.new
+    @dive_site = DiveSite.new
   end
 
   def create
-    @dive_site = Dive_site.new(dive_site_params)
-    @dive_site.user = current_user
+    @dive_site = DiveSite.new(dive_site_params)
+    # @dive_site.user = current_user
     if @dive_site.save
-      redirect dive_site_path(@dive_site)
+      redirect_to dive_site_path(@dive_site)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-
   private
 
   def dive_site_params
-    params.require[:dive_site].permit[:name, :description, :country, :address, :type]
+    params.require(:dive_site).permit(:name, :description, :country, :address, :dive_type, :photo)
   end
 end
--- app/models/dive_site.rb
+++ app/models/dive_site.rb
@@ -1,3 +1,4 @@
 class DiveSite < ApplicationRecord
   has_many :logs
+  has_one_attached :photo
 end
