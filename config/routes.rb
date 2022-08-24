51448dcbab7a8a9dc6ff1e9923e8aa34946831b5
Marving Moreton <moreton.marving@gmail.com>
2022-08-24 11:32:14 -0400

    fixing path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :dive_sites, only: [:new, :create, :index, :show] do
+  resources :dive_sites, only: [:new, :create, :index, :show], path: '/dive-sites' do
     resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   end
   resources :logs, only: [:new, :create, :index, :show, :edit, :update]
