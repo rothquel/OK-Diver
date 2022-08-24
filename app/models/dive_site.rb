d62b2dbce7816f14d61baf20f8b5126478a1cc0e
Marving Moreton <moreton.marving@gmail.com>
2022-08-24 14:21:18 -0400

    added the dropdown for Divesite TYPE & LEVEL

--- app/models/dive_site.rb
+++ app/models/dive_site.rb
@@ -10,4 +10,9 @@ class DiveSite < ApplicationRecord
   # TAGS
   has_many :dive_site_tags, dependent: :destroy
   has_many :tags, through: :dive_site_tags
+
+  # Array to define content of level # Cap = constante
+  PROFICIENCY = %w[Beginner Intermediate Advanced Professional]
+  # Array to define options of dive type # Cap = constante
+  TYPE = %w[Reef\ diving Wall\ diving Wreck\ diving Cave\ diving Night\ diving Drift\ diving]
 end
