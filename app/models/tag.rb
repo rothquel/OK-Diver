0ac545d95718b8ae2106ada15d49f23948f2dbd2
Nic Rothquel <nrothquel@gmail.com>
2022-08-24 17:13:54 -0400

    troubleshooting

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,24 +1,24 @@
 class Tag < ApplicationRecord
-  has_many :dive_site_tags, dependent: destroy
+  has_many :dive_site_tags, dependent: :destroy
 
   TAGS = %w[
-Underwater fauna
-underwater flora
-phytoplankton
-red algae
-kelp
-seagrass
-sargassum
-sharks
-Ship's wreck
-Barrier Reef
-Pipefish
-Lionfish
-Ghost Pipefish
-Seahorse
-Comet Goldfish
-Shark
-Tuna
-sea urchin
+    Underwater fauna
+    underwater flora
+    phytoplankton
+    red algae
+    kelp
+    seagrass
+    sargassum
+    sharks
+    Ship's wreck
+    Barrier Reef
+    Pipefish
+    Lionfish
+    Ghost Pipefish
+    Seahorse
+    Comet Goldfish
+    Shark
+    Tuna
+    sea urchin
   ]
 end
