898b81d7c21d5238d27fb1b005c0ca20bb8291aa
Nic Rothquel <nrothquel@gmail.com>
2022-08-24 17:07:40 -0400

    fixed controller typo

--- app/models/dive_site_tag.rb
+++ app/models/dive_site_tag.rb
@@ -1,5 +1,5 @@
 class DiveSiteTag < ApplicationRecord
-  belongs_to :dive_site_tags
-  belongs_to :Tag
+  # belongs_to :dive_site_tags wtf???
+  belongs_to :tag
   validates :tag, uniqueness: { scope: :prisoner, message: 'already taken' }
 end
