3ffa37a089fabab0874200f99933472f1ba9b634
Nic Rothquel <nrothquel@gmail.com>
2022-08-24 16:16:17 -0400

    added autocomplete to city

--- app/javascript/controllers/create_site_map_controller.js
+++ app/javascript/controllers/create_site_map_controller.js
@@ -11,9 +11,6 @@ export default class extends Controller {
   static targets = ["map", "latitude", "longitude"]
 
   connect() {
-    // console.log(this.apiKeyValue)
-    console.log(this.latitudeTarget)
-    console.log(this.longitudeTarget)
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
@@ -25,23 +22,21 @@ export default class extends Controller {
       mapboxgl: mapboxgl }))
 
     this.map.on('click', (event) => {
-      console.log(event.lngLat)
       this.latitudeTarget.value = event.lngLat.lat
       this.longitudeTarget.value = event.lngLat.lng
+      this.#addMarkerToMap(event)
     });
 
-    this.#addMarkersToMap()
   }
 
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window) // Add this
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .setPopup(popup) // Add this
-        .addTo(this.map)
-    });
+  #addMarkerToMap(event) {
+    new mapboxgl.Marker()
+    .setLngLat([event.lngLat.lng, event.lngLat.lat])
+    .addTo(this.map);
   }
+
+
+
 
 
 }
