343fc115f200a16d27b7973f6316180170a3fc29
Anais Roger <anais.roger@mail.mcgill.ca>
2022-08-28 19:01:04 -0400

    centered button on bottom cards

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_210845) do
     t.string "level"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.string "favoritable_type", null: false
+    t.bigint "favoritable_id", null: false
+    t.string "favoritor_type", null: false
+    t.bigint "favoritor_id", null: false
+    t.string "scope", default: "favorite", null: false
+    t.boolean "blocked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blocked"], name: "index_favorites_on_blocked"
+    t.index ["favoritable_id", "favoritable_type"], name: "fk_favoritables"
+    t.index ["favoritable_type", "favoritable_id", "favoritor_type", "favoritor_id", "scope"], name: "uniq_favorites__and_favoritables", unique: true
+    t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable"
+    t.index ["favoritor_id", "favoritor_type"], name: "fk_favorites"
+    t.index ["favoritor_type", "favoritor_id"], name: "index_favorites_on_favoritor"
+    t.index ["scope"], name: "index_favorites_on_scope"
+  end
+
   create_table "logs", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "dive_site_id", null: false
