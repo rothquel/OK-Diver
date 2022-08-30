91c694f3dc1bdc677e939c6d68dc791511abf3c6
Anais Roger <anais.roger@mail.mcgill.ca>
2022-08-30 12:34:30 -0400

    changed depth to meters for the depth input of the log a dive form

--- app/javascript/controllers/depth_slider_controller.js
+++ app/javascript/controllers/depth_slider_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "depth", "sliderStartDepth"]
   connect() {
-    this.depthTarget.innerHTML = `${this.sliderStartDepthTarget.value}°c`
+    this.depthTarget.innerHTML = `${this.sliderStartDepthTarget.value} m`
   }
 
   updateDepth(event) {
