df608e0714755db1ef3376215f692295fb2d1940
Marving Moreton <moreton.marving@gmail.com>
2022-08-31 13:06:12 -0400

    ongoing

--- app/controllers/dive_site_tags_controller.rb
+++ app/controllers/dive_site_tags_controller.rb
@@ -7,5 +7,10 @@ class DiveSiteTagsController < ApplicationController
   def create
     @dive_site = DiveSite.Find(params[:dive_site_id])
     @tags = Tag.where(id: params[:dive_site_tag][:tag])
+
+    @tags.each do |tag|
+      @dive_site_tag = DiveSiteTag.new(dive_site: @dive_site, tag: tag)
+      render :new, status: :unprocessable_entity and return unless @dive_site_tag.save
+    end
   end
 end
