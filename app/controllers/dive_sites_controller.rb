d3129329bbba0a0c88982ddd9a2be2704f941551
Marving Moreton <moreton.marving@gmail.com>
2022-08-31 10:44:28 -0400

    almost done

--- app/controllers/dive_sites_controller.rb
+++ app/controllers/dive_sites_controller.rb
@@ -5,7 +5,7 @@ class DiveSitesController < ApplicationController
   def index
     # Searchbar
     if params[:query].present?
-      sql_query = "name ILIKE :query OR country ILIKE :query"
+      sql_query = "country ILIKE :query"
       @dive_sites = DiveSite.where(sql_query, query: "%#{params[:query]}%")
     else
       # Thx to lazy load, active record will update dive_sites in accordance
