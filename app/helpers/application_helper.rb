b7e88c19878aa8729d753259489bc6a4ac5a6b92
Marving Moreton <moreton.marving@gmail.com>
2022-08-31 16:33:38 -0400

    design stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def want_sidebar
+    # raise
+    current_page?('/create-log') || current_page?('/logs/new') || current_page?('/dive-sites/new', check_parameters: false)
+  end
 end
